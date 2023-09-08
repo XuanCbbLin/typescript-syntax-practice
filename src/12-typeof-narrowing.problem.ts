a6dc88f105eaf75676dbc567e048f234ae7c0ccf
Xuan <xuancbblin@gmail.com>
2023-09-08 17:50:05 +0800

    feat: 12-typeof-narrowing.problem

--- src/12-typeof-narrowing.problem.ts
+++ src/12-typeof-narrowing.problem.ts
@@ -1,6 +1,22 @@
 import { expect, it } from "vitest";
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+const coerceAmount = (amount: number | { amount: number }) => {
+  
+  // Method 1:
+  if(typeof amount === "object") {
+    return amount.amount;
+  }
+
+  return amount
+
+  // Method 2:
+  // if (typeof amount === 'number') {
+  //   return amount;
+  // }
+
+  // return amount.amount;
+
+};
 
 it("Should return the amount when passed an object", () => {
   expect(coerceAmount({ amount: 20 })).toEqual(20);
